971da41cc3798ab9773821fab2e2f377c5309850
misselvexu <iskp.me@gmail.com>
2019-06-05 13:55:49 +0800

    add spring cloud app support

---
+++ template/distribution/spring-cloud-app/src/main/java/CloudApplicationBootstrap.java
@@ -0,0 +1,29 @@
+package ${__package__};
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import xyz.vopen.tiffany.swagger.EnableSwagger2;
+
+/**
+ * {@link BootApplicationBootstrap}
+ *
+ * @author <a href="mailto:iskp.me@gmail.com">Elve.Xu</a>
+ * @version ${project.version} - 2019-03-12.
+ */
+@EnableSwagger2
+@SpringBootApplication
+@EnableDiscoveryClient
+public class CloudApplicationBootstrap {
+
+  public static void main(String[] args) {
+    // new application
+    new SpringApplicationBuilder()
+        .sources(CloudApplicationBootstrap.class)
+
+        // default properties
+        .properties("spring.profiles.active=nacos")
+        .web(WebApplicationType.SERVLET)
+        .run(args);
+  }
+}
