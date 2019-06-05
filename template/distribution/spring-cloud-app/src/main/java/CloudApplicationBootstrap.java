2426a44c280f9a4cedcfa54148ac66c7fce26643
misselvexu <iskp.me@gmail.com>
2019-06-05 15:30:58 +0800

    fix missing package import issues

--- template/distribution/spring-cloud-app/src/main/java/CloudApplicationBootstrap.java
+++ template/distribution/spring-cloud-app/src/main/java/CloudApplicationBootstrap.java
@@ -1,7 +1,7 @@
 package ${__package__};
 
 import org.springframework.boot.WebApplicationType;
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import xyz.vopen.tiffany.swagger.EnableSwagger2;
